76717acb4f2615fe5fba0059358a3152329c35c9
polowis <tranphuochung0912@gmail.com>
2020-11-28 18:44:54 +1100

    refactor: split navbar component

--- app/resources/js/components/auth/LoginComponent.vue
+++ app/resources/js/components/auth/LoginComponent.vue
@@ -1,67 +1,6 @@
 <template>
     <div>
-        <nav class="navbar" role="navigation" aria-label="main navigation">
-                    <div class="navbar-brand">
-                      <a class="navbar-item" href="/">
-                        <img src="/static/img/logo.png" width="112" height="28">
-                      </a>
-                  
-                      <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                      </a>
-                    </div>
-                  
-                    <div id="navbarBasicExample" class="navbar-menu">
-                      <div class="navbar-start">
-                        <a class="navbar-item" href="/">
-                          Home
-                        </a>
-                  
-                        <a class="navbar-item">
-                          Documentation
-                        </a>
-                  
-                        <div class="navbar-item has-dropdown is-hoverable">
-                          <a class="navbar-link">
-                            More
-                          </a>
-                  
-                          <div class="navbar-dropdown">
-                            <a class="navbar-item">
-                              About
-                            </a>
-                            <a class="navbar-item">
-                              Jobs
-                            </a>
-                            <a class="navbar-item">
-                              Contact
-                            </a>
-                            <hr class="navbar-divider">
-                            <a class="navbar-item">
-                              Report an issue
-                            </a>
-                          </div>
-                        </div>
-                      </div>
-                  
-                      <div class="navbar-end">
-                        <div class="navbar-item">
-                          <div class="buttons">
-                            <a class="button is-primary" href="/register">
-                              <strong>Sign up</strong>
-                            </a>
-                            <a class="button is-light" href="/login">
-                              Log in
-                            </a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </nav>
-                
-
+        <NavbarComponent></NavbarComponent>
             <div class="form">
             <h3 id="form-header">Log in</h3>
             <div class="field">
@@ -91,8 +30,11 @@
     </div>
 </template>
 <script>
+import NavbarComponent from '../NavbarComponent'
 export default {
-    
+    components: {
+        NavbarComponent
+    }
 }
 </script>
 <style scoped>
