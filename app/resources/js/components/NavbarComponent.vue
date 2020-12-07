f9c12f18cec0cc1ff52f872f861be8401b442d0c
polowis <tranphuochung0912@gmail.com>
2020-12-07 17:23:55 +1100

    feat: new navbar ui

--- app/resources/js/components/NavbarComponent.vue
+++ app/resources/js/components/NavbarComponent.vue
@@ -6,7 +6,7 @@
                         <img src="/static/img/logo.png" width="112" height="28">
                       </a>
                   
-                      <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                      <a role="button" class="navbar-burger burger" style="color:white" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
                         <span aria-hidden="true"></span>
                         <span aria-hidden="true"></span>
                         <span aria-hidden="true"></span>
@@ -49,10 +49,11 @@
                     <div class="navbar-end" v-if="user.id != 'none'">
                         <div class="navbar-item">
                             <div class="buttons">
+                                
+                                <a class="button is-primary">Hi {{this.user.username}}</a>
                                 <a class="button is-danger" @click.prevent="logout()">
                                     <strong>Log out</strong>
                                 </a>
-                                <a class="button is-primary">Hi {{this.user.username}}</a>
                             </div>
                         </div>
                      </div>
@@ -104,3 +105,41 @@ export default {
     }
 }
 </script>
+<style scoped>
+.navbar {
+  background-color: #63736e;
+}
+
+.navbar-item {
+  background-color: #63736e;
+  color: white;
+}
+
+a.navbar-item:hover{
+  background-color: #3a4441;
+}
+
+a.navbar-link:hover{
+  background-color: #3a4441;
+}
+.navbar-menu {
+  background-color: #63736e;
+}
+
+.navbar-link {
+  color: #f9ffcb;
+}
+
+.navbar-dropdown {
+  background-color: #63736e;
+  border-top: 1px solid #63736e;
+}
+
+.navbar-divider {
+   background-color: #63736e;
+}
+
+.has-dropdown{
+  background-color: #63736e;
+}
+</style>
