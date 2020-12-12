2ff21d88711fdada2393aab52f44e7e4a6951e21
polowis <tranphuochung0912@gmail.com>
2020-12-12 16:20:33 +1100

    feat: add template view for specific land

---
+++ app/resources/js/components/core/land/LandView.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <NavbarComponent></NavbarComponent>
+        <div class="page">
+            <div class="app-content">
+                <div class="land-photo">
+                    </div>
+                <div class="land-details">
+                    <div>Hello</div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+<script>
+import NavbarComponent from '../../NavbarComponent'
+export default {
+    components: {
+        NavbarComponent
+    },
+
+}
+</script>
+
+<style scoped>
+.land-photo {
+    width: 200px;
+    float: left;
+}
+
+.land-details{
+    width: 200px;
+    float:right;
+}
+</style>
