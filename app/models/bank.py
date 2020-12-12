27930a64f1fbd8c76d78d2ffc038da0266709e28
polowis <tranphuochung0912@gmail.com>
2020-12-12 18:20:03 +1100

    feat: add model to store commmunity and owner money

---
+++ app/models/bank.py
@@ -0,0 +1,11 @@
+
+
+from django.db import models
+
+class CommunityBank(models.Model):
+    balance = models.DecimalField(max_digits=30, decimal_places=4)
+
+
+
+class OwnerBank(models.Model):
+    balance = models.DecimalField(max_digits=30, decimal_places=4)
