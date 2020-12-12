634b95df74af108f81cf8fdc36055e5d0a5f73e1
polowis <tranphuochung0912@gmail.com>
2020-12-12 17:00:50 +1100

    feat: add frontend vue to display specific land when request

--- app/resources/js/components/core/land/LandView.vue
+++ app/resources/js/components/core/land/LandView.vue
@@ -4,9 +4,19 @@
         <div class="page">
             <div class="app-content">
                 <div class="land-photo">
+                    <figure class="image">
+                    <img src="/static/img/land.png" class="land-image">
+                    </figure>
                     </div>
                 <div class="land-details">
-                    <div>Hello</div>
+                    <h3>Land ID: {{land.land_id}}</h3>
+                    
+                    <p>Company Owner: {{land.company_name}}</p>
+                    <p>Land Status: {{land.status}}</p>
+                    <p>Land Level: {{land.level}}</p>
+                    <p>Land Cost: {{land.buy_cost}}</p>
+                    <p>Land Rent: {{land.rent_cost}}</p>
+                    <p v-if="land.company_name === null" style="color: #00d1b2">Available to buy/rent</p>
                 </div>
             </div>
 
@@ -20,17 +30,43 @@ export default {
         NavbarComponent
     },
 
+    data() {
+        return {
+            land: ""
+        }
+    },
+
+    created() {
+        this.fetchLandDetail()
+    },
+
+    methods: {
+        fetchLandDetail() {
+            axios.post((location.pathname+location.search)).then(response => {
+                let data = response.data
+                this.land = data
+                console.log(data.company_name)
+            })
+        }
+    }
+
 }
 </script>
 
 <style scoped>
 .land-photo {
-    width: 200px;
+    width: 500px;
     float: left;
+    margin-left: 20px;
 }
 
 .land-details{
-    width: 200px;
+    width: 500px;
     float:right;
+    margin-right: 20px;
+}
+.land-image{
+    width: 500px; 
+    height: 300px
 }
 </style>
