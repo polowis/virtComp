b2fd5252ae0122f5205965352cc71e5a330dffea
polowis <tranphuochung0912@gmail.com>
2020-12-13 08:17:53 +1100

    refactor: split code

--- app/views/core/company/land/land_buy.py
+++ app/views/core/company/land/land_buy.py
@@ -8,10 +8,18 @@ class LandBuy(View):
         try:
             land: LandOwn = LandOwn.objects.get(land_id=land_id)
             company: Company = get_current_register_company(request)
-            if company.balance >= land.buy_cost:
-                new_balance = company.balance - land.buy_cost
-                company.balance = new_balance
-                company.save()
+            if land.company_name is not None:
+                self.buy(land, company)
+            else:
+                data = {'error': True, 'message': "A company has owned this land"}
+                return JsonResponse(data)
         except:
-            data = {'error': 'Not Found'}
+            data = {'error': True, 'message': 'Not found'}
             return JsonResponse(data)
+    
+
+    def buy(self, land: LandOwn, company: Company):
+        if company.balance >= land.buy_cost:
+            new_balance = company.balance - land.buy_cost
+            company.balance = new_balance
+            company.save()
