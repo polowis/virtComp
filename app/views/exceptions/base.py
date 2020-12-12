15ad9846021f3687b77cec39a2b635971f360865
polowis <tranphuochung0912@gmail.com>
2020-12-12 19:40:46 +1100

    feat: add custom view for 500 status code

--- app/views/exceptions/base.py
+++ app/views/exceptions/base.py
@@ -11,3 +11,9 @@ def Error404(request: HttpRequest, exception=None):
     template_name = 'exceptions/404.html'
     template = render_to_string(template_name)
     return HttpResponse(template, status=404)
+
+
+def Error500(request: HttpRequest, exception=None):
+    template_name = 'exceptions/500.html'
+    template = render_to_string(template_name)
+    return HttpResponse(template, status=500)
