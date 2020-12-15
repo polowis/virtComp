9ddbc331a25738a1612e1ad6ec0e802188033577
polowis <tranphuochung0912@gmail.com>
2020-12-15 13:54:11 +1100

    refactor: remove unused functions

--- app/management/commands/bulk_create_land.py
+++ app/management/commands/bulk_create_land.py
@@ -10,13 +10,6 @@ class Command(BaseCommand):
         parser.add_argument('land_continent', nargs='?', type=str,
                             default='asia')
 
-    def get_probability(self):
-        return [0.23, 0.15, 0.13, 0.1, 0.09, 0.08, 0.06, 0.06, 0.05,
-                0.03, 0.02]
-    
-    def get_land_level(self):
-        return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
     def handle(self, *args, **options):
         number_of_lands = options['number_of_lands']
         continent = options['land_continent']
