957136b4044ebba61cc88ae21c6117d8df7a209d
polowis <tranphuochung0912@gmail.com>
2020-12-17 18:12:01 +1100

    feat: add function to transform land continent to uppercase

--- app/resources/js/components/core/land/LandView.vue
+++ app/resources/js/components/core/land/LandView.vue
@@ -17,6 +17,7 @@
                     <p>Land Level: {{land.level}}</p>
                     <p>Land Cost: ${{land.buy_cost}}</p>
                     <p>Land Rent: ${{land.rent_cost}} / week</p>
+                    <p>Land Continent: {{this.titleCase(land.continent)}}</p>
                     <p v-if="land.company_name === null" style="color: #00d1b2">Available to buy/rent</p>
                     <div v-if="land.company_name === null">
                         <div class="control">
@@ -59,7 +60,17 @@ export default {
             axios.post(`/land/${this.land.land_id}/buy`).then(response => {
 
             })
-        }
+        },
+
+        titleCase(str) {
+            let splitString = str.toLowerCase().split(' ');
+            for (let i = 0; i < splitString.length; i++) {
+               
+                splitString[i] = splitString[i].charAt(0).toUpperCase() + splitString[i].substring(1);     
+            }
+            return splitString.join(' '); 
+        },
+
     }
 
 }
