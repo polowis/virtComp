c09b52d9ee7eb799ffdb7c31cd085839ee04c133
polowis <tranphuochung0912@gmail.com>
2020-12-19 18:35:44 +1100

    fix

--- setting/settings.py
+++ setting/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app.core.middleware.company_logged_in.CompanyLoggedInMiddleware',
     'app',
-    'app.core.middleware.company_logged_in.CompanyLoggedInMiddleware'
 ]
 
 MIDDLEWARE = [
