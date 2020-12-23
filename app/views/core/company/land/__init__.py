65e0d82a0f4ca635dd599ebc7b264e10e67bb3c8
polowis <tranphuochung0912@gmail.com>
2020-12-24 09:59:59 +1100

    feat: add rent land view function

--- app/views/core/company/land/__init__.py
+++ app/views/core/company/land/__init__.py
@@ -1,2 +1,3 @@
 from .land_view import * # noqa
 from .land_buy import * # noqa
+from .land_rent import * # noqa
---
+++ app/views/core/company/land/land_rent.py
@@ -0,0 +1,27 @@
+from django.views import View
+from django.http import HttpRequest, JsonResponse, Http404
+from app.models import Landscape, Company
+
+
+class LandRent(View):
+    def post(self, request: HttpRequest, land_id=None):
+        try:
+            land: Landscape = Landscape.objects.get(land_id=land_id)
+            company: Company = request.company
+            if land.can_be_purchased() and company is not None:
+                if company.can_own_landscape(land, 'buy'):
+                    company.rent_landscape(land)
+                    if land.company_name == company.company_name:
+                        data = {'error': False, 'message': 'Successfully Rent Landscape',
+                                'redirect_url': '/company/'}
+                        return JsonResponse(data)
+                data = {'error': True, 'message': "Insufficient amount of money"}
+                return JsonResponse(data)
+            data = {'error': True, 'message': "A company has owned this land"}
+            return JsonResponse(data)
+        except Landscape.DoesNotExist:
+            data = {'error': True, 'message': 'Not found'}
+            return JsonResponse(data)
+    
+    def get(self, request: HttpRequest, land_id=None):
+        raise Http404()
