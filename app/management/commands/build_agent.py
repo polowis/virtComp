0ddb70846e5db9c39254cd64b93295d07166724c
polowis <tranphuochung0912@gmail.com>
2021-01-02 18:24:32 +1100

    feat: add command file to create agent

---
+++ app/management/commands/build_agent.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand, CommandError
+from app.core.services.builders.agent_builder import AgentBuilder
+
+
+class Command(BaseCommand):
+
+    help = 'Create a new agent'
+
+    def add_arguments(self, parser):
+        parser.add_argument('number_of_agent', nargs='+', type=int)
+        parser.add_argument('--continent', nargs='?', type=str,
+                            default='alantica')
+    
+    def handle(self, *args, **options):
+        number_of_agents: int = options.get('number_of_agents', 10)
+        continent: str = options.get('continent', 'alantica')
+        try:
+            for agent in range(number_of_agents):
+                builder = AgentBuilder(continent=continent)
+                builder.build()
+        except Exception as e:
+            raise CommandError(e)
