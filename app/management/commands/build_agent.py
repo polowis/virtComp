858bdf191b7f93d7a9e40e581180d671cfb60d99
polowis <tranphuochung0912@gmail.com>
2021-01-02 18:37:58 +1100

    fix: fix minor issue that not affects the code

--- app/management/commands/build_agent.py
+++ app/management/commands/build_agent.py
@@ -6,14 +6,16 @@ class Command(BaseCommand):
 
     help = 'Create a new agent'
 
+    default_continent = 'alantica'
+
     def add_arguments(self, parser):
         parser.add_argument('number_of_agent', nargs='+', type=int)
         parser.add_argument('--continent', nargs='?', type=str,
-                            default='alantica')
+                            default=self.default_continent)
     
     def handle(self, *args, **options):
         number_of_agents: int = options.get('number_of_agents', 10)
-        continent: str = options.get('continent', 'alantica')
+        continent: str = options.get('continent', self.default_continent)
         try:
             for agent in range(number_of_agents):
                 builder = AgentBuilder(continent=continent)
