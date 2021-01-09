cdd1f2c6ada25a9bbced3956f77a85f641fa63b0
polowis <tranphuochung0912@gmail.com>
2021-01-09 20:26:30 +1100

    fix: add threading when loading data to imrpove speed

--- app/management/commands/load_data.py
+++ app/management/commands/load_data.py
@@ -1,15 +1,25 @@
 from django.core.management.base import BaseCommand, CommandError
 import os
-    
+import concurrent.futures
+import threading
 
 class Command(BaseCommand):
     help = 'Load all data from csv file to database'
 
+    command_list = ['load_land', 'load_building', 'load_place']
+
     
     def handle(self, *args, **options):
         try:
-            os.system('./manage.py load_land')
-            os.system('./manage.py load_building')
-            os.system('./manage.py load_place')
+            self.load_data(self.command_list)
         except Exception as e:
             raise CommandError(e)
+
+    def load_command(self, command):
+        os.system(f'./manage.py {command}')
+
+    def load_data(self, commands):
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            executor.map(self.load_command, commands)
+    
+    
