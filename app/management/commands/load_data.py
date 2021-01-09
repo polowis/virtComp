c702cc73042e97cfe5bdaeb96f23e749df97c8c2
polowis <tranphuochung0912@gmail.com>
2021-01-09 20:34:01 +1100

    style: add help time command to display time taken when exceuting code

--- app/management/commands/load_data.py
+++ app/management/commands/load_data.py
@@ -1,7 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
 import os
 import concurrent.futures
-import threading
+import time
+
 
 class Command(BaseCommand):
     help = 'Load all data from csv file to database'
@@ -10,8 +11,11 @@ class Command(BaseCommand):
 
     
     def handle(self, *args, **options):
+        start = time.time()
         try:
             self.load_data(self.command_list)
+            end = time.time()
+            print(f"Loaded completed in: {round(end-start, 4)}s")
         except Exception as e:
             raise CommandError(e)
 
