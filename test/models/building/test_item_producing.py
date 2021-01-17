ddc9e1d2f4f423eb927bd0d620b40ecc3df9e359
polowis <tranphuochung0912@gmail.com>
2021-01-17 16:35:33 +1100

    test: intially add test

---
+++ test/models/building/test_item_producing.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from app.models import Landscape, Land, Company, Building, BuildingType, Item
+
+
+class BuildingItemProducingTestCase(TestCase):
+    def setUp(self):
+        Land.objects.load_land('csv_data/landData.csv')
+        BuildingType.objects.load_building_type('csv_data/buildingType.csv')
+        Item.objects.load_items('csv_data/item.csv')
+        self.land: Landscape = Landscape.objects.create_land()
+        self.user: User = User.objects.create_user('johnyTest', 'john@example.com', 'johnpassword')
+        self.company: Company = Company.objects.create_company('johnCompany', self.user)
+        self.company.balance = self.land.buy_cost + 1
+        self.land.purchase_landscape(self.company)
+        building: BuildingType = BuildingType.objects.get_building_by_type('supreme mine')
+        self.company.balance = building.get_rent_cost(0)
+        self.test_building = Building.objects.create_building("supreme mine", 'mymine', self.company,
+                                                              'rent', 0, self.land)
