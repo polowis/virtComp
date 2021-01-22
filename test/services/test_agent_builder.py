ef16328a3c67725d9a56ea75c8d8ae19acc71586
polowis <tranphuochung0912@gmail.com>
2021-01-23 09:29:55 +1100

    test: add test case for building many agents

--- test/services/test_agent_builder.py
+++ test/services/test_agent_builder.py
@@ -14,5 +14,13 @@ class AgentBuilderTestCase(TestCase):
         builder.debug = False
         builder.continent = Land.objects.default_continent()
         agent = builder.build()
-
         self.assertEqual(agent.name, 'testJohny')
+    
+    def test_build_many_agent_without_generator(self):
+        builder: AgentBuilder = AgentBuilder(use_generator=False)
+        builder.debug = False
+        builder.continent = Land.objects.default_continent()
+        agents = builder.build_many_agents(2, ['johny', 'alice'])
+        self.assertEqual(agents[0].name, 'johny')
+        self.assertEqual(agents[1].name, 'alice')
+
