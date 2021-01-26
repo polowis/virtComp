a47b51ec55c98a5c4c88a749ea2baa2bf220bcdf
polowis <tranphuochung0912@gmail.com>
2021-01-26 20:52:20 +1100

    refactor: splitng building builder class

--- app/models/core/building.py
+++ app/models/core/building.py
@@ -6,129 +6,13 @@ from .landscape import Landscape
 from app.models.constants import BuildingType
 import random
 from app.models.core.exception import CannotBuyBuildingOnRentLandscape
-from app.models.core.exception import NegativeLevel, UnableToConstructBuilding
+from app.models.core.exception import UnableToConstructBuilding
 from typing import Union
 from django.utils import timezone
 from django.core.exceptions import ObjectDoesNotExist
 from datetime import datetime, timedelta
 from app.models.constants import Item
-
-
-class BuildingBuilder(object):
-    """
-    This is just for constructing the building. For buying the building.
-    Please see alternative method
-
-    It will include the cost of constructing the building.
-
-    Usage: call construct_building()
-
-    Constructor: Landscape instance. The land to constructing the building on top
-    """
-    def __init__(self, landscape: Landscape, level: int, building_type: str, building_name: str):
-        """
-        Constructor: Landscape instance. The land to constructing the building on top
-        """
-        self.landscape = landscape
-        self.level = level
-        self.building_type = building_type
-        self.building_name = building_name
-        self.buildingType_instance = None
-        self.method_acquired = None
-
-    def can_build(self, company: Company, method_acquired: str) -> bool:
-        """
-        Return true if can build the building on this landscape
-        
-        This method only check for whether or not the
-        company able to construct the building with given lEVEL and company owner
-        assocciated with landscape.
-
-        This method does not check for anything else
-        """
-        self.method_acquired = method_acquired
-        if self.landscape.owned_by(company):
-            if method_acquired == 'rent':
-                return self.landscape.can_create_rent_building(self.level)
-            if method_acquired == 'buy':
-                return self.landscape.can_create_building(self.level)
-        return False
-    
-    def get_building_details(self) -> Union[BuildingType, None]:
-        """
-        Return the buildingtype instance of None if none is found
-        """
-        try:
-            return BuildingType.objects.get_building_by_type(self.building_type)
-        except BuildingType.DoesNotExist:
-            return None
-    
-    def construct_building(self, company: Company, method_acquired: str):
-        """
-        Construct the building. Only use this method to construct the building
-        regardless of method of acquisition
-        """
-        if self.can_build(company, method_acquired):
-            try:
-                self.process_transaction(method_acquired)
-                return self.create_building()
-            except ValueError:
-                raise UnableToConstructBuilding()
-        else:
-            raise UnableToConstructBuilding()
-    
-    def process_transaction(self, method_acquired: str):
-        """
-        Proccessing transaction upon owning this building. Do not call this method directly.
-        
-        Accept one parameter: method_acquired
-
-        raise ValueError
-        """
-        building_type: BuildingType = self.get_building_details()
-        if building_type is not None:
-            self.buildingType_instance = building_type
-            try:
-                cost = building_type.get_cost(method_acquired.lower(), self.level)
-                self.cost = cost
-                company = self.landscape.company
-                company.balance -= cost
-                if company.balance < 0:
-                    raise ValueError("Company balance must be positive")
-                company.save()
-            except NegativeLevel:
-                raise ValueError("The level must be negative")
-
-    def get_building_details_as_dict(self) -> dict:
-        """
-        Return the dictionary of buildingtyp details
-        If you're looking for an instance, refer to get_building_details() instead
-        """
-        details: dict = {
-            'building_type': self.building_type,
-            'building_name': self.building_name,
-            'landscape': self.landscape,
-            'company': self.landscape.company,
-            'company_name': self.landscape.company_name,
-            'current_level': self.level,
-            'max_storage': self.buildingType_instance.get_max_storage(self.level),
-            'max_employees': self.buildingType_instance.get_max_employees(self.level),
-            'is_rent': self.method_acquired == 'rent',
-            'is_buy': self.method_acquired == 'buy',
-            #  the price that when first bought this building regardless of method of acquiring
-            'buy_cost': self.cost,
-            'rent_cost': self.cost if self.method_acquired == 'rent' else None,
-            'last_collected_money_at': timezone.now()
-        }
-        return details
-
-    def create_building(self) -> Building:
-        """Create the building and save to the database.
-        Do not call this method directly.
-        """
-        details = self.get_building_details_as_dict()
-        building: Building = Building.objects.create(**details)
-        return building
+from app.core.services.builders.building_builder import BuildingBuilder
 
 
 class BuildingManager(models.Manager):
@@ -136,7 +20,10 @@ class BuildingManager(models.Manager):
 
     def create_building(self, building_type: str, building_name: str, company: Company,
                         method_acquired: str, level: int, landscape: Landscape) -> Building:
-        """Call this function to create a building with the given type and name
+        """
+        DEPRECATED: DO NOT USE THIS METHOD. USE BuildingBuilder class instead
+
+        Call this function to create a building with the given type and name
         And the company instance that owns the building regardless of acquisition methods
 
         NOTE: The given landscape must own by the given company
@@ -147,6 +34,7 @@ class BuildingManager(models.Manager):
         throw exception: CannotBuyBuildingOnRentLandscape, UnableToConstructBuilding
 
         return Building instance
+
         """
 
         supported_methods_acquired = ['buy', 'rent']
