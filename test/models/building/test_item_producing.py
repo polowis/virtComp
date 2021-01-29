d2fb2ed9add1d98ffc2f8338f6ffc65ce6b3555b
polowis <tranphuochung0912@gmail.com>
2021-01-29 14:14:50 +1100

    test: adjust test to load place first

--- test/models/building/test_item_producing.py
+++ test/models/building/test_item_producing.py
@@ -1,14 +1,12 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from app.models import Landscape, Land, Company, BuildingType, Item
+from app.models import Landscape, Land, Company, BuildingType, Item, Place
 from app.core.services.builders.building_builder import BuildingBuilder
 
 
 class BuildingItemProducingTestCase(TestCase):
     def setUp(self):
-        Land.objects.load_land('csv_data/landData.csv')
-        BuildingType.objects.load_building_type('csv_data/buildingType.csv')
-        Item.objects.load_items('csv_data/item.csv')
+        self.load_data()
         self.land: Landscape = Landscape.objects.create_land()
         self.user: User = User.objects.create_user('johnyTest', 'john@example.com', 'johnpassword')
         self.company: Company = Company.objects.create_company('johnCompany', self.user)
@@ -18,7 +16,13 @@ class BuildingItemProducingTestCase(TestCase):
         self.company.balance = building.get_rent_cost(0)
         self.building = BuildingBuilder.construct("supreme mine", 'mymine', self.company,
                                                   'rent', 0, self.land)
-    
+
+    def load_data(self):
+        Place.objects.load_data('csv_data/place.csv')
+        Land.objects.load_land('csv_data/landData.csv')
+        BuildingType.objects.load_building_type('csv_data/buildingType.csv')
+        Item.objects.load_items('csv_data/item.csv')
+
     def test_building_can_produce(self):
         item = Item.objects.get(name='diamond')  # assuming diamond is produced from surpeme mine
         self.assertEqual(self.building.can_produce(item), True)
