dcd13110f8c8322bf4580b2318e6b62208be2dd1
polowis <tranphuochung0912@gmail.com>
2021-02-06 19:07:23 +1100

    fix: typo :(

--- app/views/api/v1/company/company_.py
+++ app/views/api/v1/company/company_.py
@@ -31,12 +31,14 @@ class CompanyView(View, UserLoggedInRequiredMixinJSON):
             continent = request.POST.get('continent', None)
             if company_name is None:
                 data = {'error': True, 'message': 'missing company name'}
-                if Company.objects.can_create_company(company_name, continent):
-                    try:
-                        Company.objects.create_company(company_name, request.user, continent)
-                        data = {'error': False, 'redirect_url': '/home/'}
-                        return JsonResponse(data)
-                    except Exception:
-                        data = {'error': True, 'message': 'Unable to create company'}
-        
+                return JsonResponse(data)
+            if Company.objects.can_create_company(company_name, continent):
+                try:
+                    Company.objects.create_company(company_name, request.user, continent)
+                    data = {'error': False, 'message': 'sucessfully created', 'redirect_url': '/home/'}
+                    return JsonResponse(data)
+                except Exception:
+                    data = {'error': True, 'message': 'Unable to create company'}
+                    return JsonResponse(data)
+            data = {'error': True, 'message': 'Unable to create company'}
             return JsonResponse(data)
