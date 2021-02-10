e34f9624bd6d69b5411e88f49c9cf97f1ef68d73
polowis <tranphuochung0912@gmail.com>
2021-02-10 11:03:07 +1100

    feat: add method to put item back to original storage

--- app/models/core/product/storage.py
+++ app/models/core/product/storage.py
@@ -2,6 +2,7 @@ from django.db import models
 from app.models.core.building import Building
 from django.utils import timezone
 from app.models.core.product import ProducedItem
+from typing import List
 
 
 class StorageManager(models.Manager):
@@ -55,7 +56,36 @@ class Storage(models.Model):
             return ProducedItem.objects.filter(name=item, building=self.building)
         return ProducedItem.objects.filter(name=item, building=self.building)
     
-    def update_capacity(self):
+    def update_capacity(self, current_capacity: int = None):
+        """Update the current capacity of the storage
+        But not recommended to use this method since it will try to count all the items in
+        the storage which might result in low performance.
+        """
+        if current_capacity is not None:
+            self.current_capacity = current_capacity
+            return
+        
+        # we only count those item that are not in the global store
         capacity = ProducedItem.objects.filter(building=self.building, in_global_store=False).count()
         self.current_capacity += capacity
+        self.save()
+    
+    def _reaches_max_capacity(self, current_capacity: int = None) -> bool:
+        """
+        Return True if the storage reach the max_capacity given the current capacity
+        """
+        if current_capacity is None:
+            current_capacity = self.current_capacity
+        return current_capacity > self.max_capacity
+
+    def put_item_back_to_storage(self, items: List[ProducedItem]):
+        """If the item is already in the global store, put it back to storage
+        But watch for capacity might reach max capacity.
+        """
+        items_length = len(items)
+        # only loop for
+        items = items[1:items_length]
+        items_id = [item.id for item in items]
+        ProducedItem.objects.filter(id__in=items_id).update(in_global_store=False)
+        self.current_capacity += items_length
         self.save()
